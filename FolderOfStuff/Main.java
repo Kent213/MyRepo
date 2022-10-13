4ae9f96936eff90e064806c12a173be874c44717
Kent213 <khocaoglu54@ladueschools.net>
2022-10-13 13:09:47 +0000

    Cried

---
+++ FolderOfStuff/Main.java
@@ -0,0 +1,8 @@
+package FolderOfStuff;
+import javax.sql.rowset.spi.SyncResolver;
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("Sayam is pretty cool");
+    }
+}
