d6cd642e466afa6d01d1afb5a83a1c31973a284a
Kent213 <khocaoglu54@ladueschools.net>
2022-11-02 15:17:19 +0000

    Outline

--- website/Calculator.java
+++ website/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Calculator{
 /*
     TO-DO
@@ -7,10 +8,47 @@ class Calculator{
  * Division
  * Addition
  * Subtraction
- * Recursion
+ * Recursion - ?
  */
+Scanner scan = new Scanner(System.in);
+private double num;
 
- 
+public void calculate(){
+String equation = scan.nextLine();
 
+}
+public void checkParenthesis(){
 
+}
+public double doAddition(){
+
+return num;
+}
+
+public double doSubtraction(){
+    return num;
+
+}
+public double doMultiplication(){
+    return num;
+
+}
+public double doDivision(){
+    return num;
+
+}
+public double doExponent(){
+    return num;
+}
+public void doMath(){
+//might need to say string.doAddition, string.doSubtraction, etc
+doAddition();
+doSubtraction();
+doMultiplication();
+doDivision();
+doExponent();
+}
+public static void main(String[] args){
+calculate();
+}
 }
