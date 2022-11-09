7c029f9d167cfc10c2d0146c5a43348474a0b85d
Kent213 <khocaoglu54@ladueschools.net>
2022-11-09 03:11:02 +0000

    Almost done

--- website/Calculator.java
+++ website/Calculator.java
@@ -14,7 +14,8 @@ static Scanner scan = new Scanner(System.in);
 private static double firstNum;
 private static double secondNum;
 private static double answer;
-private static int temp;
+private static int firstIndex;
+private static int secondIndex;
 static String equation;
 //have not used arrays below
 static String[] numbers = new String[]{"1","2","3","4","5","6","7","8","9","0","."};
@@ -47,72 +48,344 @@ public static void checkParenthesis(){
 
 }
 
+public static void quadraticSolver(){
+System.out.println("What would you like to calculate?");
+equation = scan.nextLine();
+equation = equation.replace(" ", "");
+    double a = 1;
+    double b = 1;
+    double c = 1;
+
+    //find a
+    firstIndex = equation.indexOf("x^2")-1;
+    while(Character.isDigit(equation.charAt(firstIndex))||equation.charAt(firstIndex)==('.')){
+        if(firstIndex==1){
+            firstIndex--;
+            break;
+        } 
+        else if(firstIndex>0){
+            firstIndex--;
+        }
+        else{
+            break;
+        }
+    }
+
+    if(!equation.substring(firstIndex, equation.indexOf("x^2")).equals("")){
+        a = Double.parseDouble(equation.substring(firstIndex, equation.indexOf("x^2")));
+    }
+
+    System.out.println("a: " + a);
+    equation = equation.substring(equation.indexOf("x")+3,equation.length());
+    System.out.println("Equation: " + equation);
+
+    //find b
+    firstIndex = equation.indexOf("x")-1;
+    while(Character.isDigit(equation.charAt(firstIndex))||equation.charAt(firstIndex)==('.')){
+        if(firstIndex==1){
+            firstIndex--;
+            break;
+        } 
+        else if(firstIndex>0){
+            firstIndex--;
+        }
+        else{
+            break;
+        }
+    }
+
+    if(!equation.substring(firstIndex, equation.indexOf("x")).equals("")){
+    b = Double.parseDouble(equation.substring(firstIndex, equation.indexOf("x")));
+    }
+    System.out.println("b: " + b);
+    equation = equation.substring(equation.indexOf("x")+1,equation.length());
+    System.out.println("Equation: " + equation);
+
+    //find c
+    if(!equation.substring(firstIndex, equation.indexOf("x^2")).equals("")){
+    c = Double.parseDouble(equation);
+    }
+    System.out.println("c: " + c);
+
+    //formula
+    double root1 = -b+Math.sqrt(b*b-4*a*c)/(2*a);
+    double root2 = -b-Math.sqrt(b*b-4*a*c)/(2*a);
+    System.out.println("root1: " + root1);
+    System.out.println("root2: " + root2);
+
+}
+
 public static void doAddition(){
-    if(equation.indexOf('+')!=-1){
-    temp = equation.indexOf('+')-1;
-
+    boolean beginning = false;
+    while(equation.indexOf('+')!=-1){
+        firstIndex = equation.indexOf('+')-1;
         //finds end index of first number
-        while(Character.isDigit(equation.charAt(temp))||equation.substring(temp--,temp).equals(".")){
-            if(temp-1>=0){
-                temp--;
+        while(Character.isDigit(equation.charAt(firstIndex))||equation.charAt(firstIndex)==('.')){
+            if(firstIndex<=1){
+                firstIndex--;
+                break;
+            } 
+            else if(firstIndex>0){
+                firstIndex--;
+            }
+            else{
+                break;
+            }
+        }
+        if(firstIndex == 0){
+            beginning = true;
+        }
+        //creates first number
+        firstNum = Double.parseDouble(equation.substring(firstIndex, equation.indexOf('+')));
+        System.out.println("firstIndex = " + firstIndex);
+        System.out.println("firstNum = " + firstNum);
+
+        secondIndex = equation.indexOf('+')+1;
+        //finds end index of first number
+        while(Character.isDigit(equation.charAt(secondIndex))||equation.charAt(secondIndex)==('.')||equation.charAt(secondIndex)==('-')){
+            if(secondIndex==equation.length()-1){
+                secondIndex++;
+                break;
+            }
+            else if(secondIndex<equation.length()){
+                secondIndex++;
             } 
             else{
                 break;
             }
             //might need more conditions for else - break statement
         }
-        //creates first number
-        firstNum = Double.parseDouble(equation.substring(temp, equation.indexOf('+')));
-        System.out.println("1st = " + firstNum);
+        //creates second number
+        secondNum = Double.parseDouble(equation.substring(equation.indexOf('+')+1,secondIndex));
+        System.out.println("secondIndex = " + secondIndex);
+        System.out.println("secondNum = " + secondNum);
 
-        temp = equation.indexOf('+')+1;
-
-        //finds end index of first number
-        while(Character.isDigit(equation.charAt(temp))||equation.substring(temp,temp++).equals(".")){
-            if(temp++<equation.length()-1){
-                temp++;
-            System.out.println("temp = " + temp);
-            } 
-            else{
-                break;
-            }
-            //might need more conditions for else - break statement
-        }
-        //creates first number
-        secondNum = Double.parseDouble(equation.substring(equation.indexOf('+')+1,temp));
-        System.out.println("2nd = " + secondNum);
-        answer = firstNum+=secondNum;
+        //adds two numbers
+        answer = (firstNum+secondNum);
         System.out.println("answer = " + answer);
+        System.out.println("firstIndex: " + firstIndex + " " + "secondIndex: " + secondIndex + " " + "Boolean: " + beginning);
 
+        //rewrites equation
+            if(!beginning){
+            equation = equation.substring(0,firstIndex+1)+answer+equation.substring(secondIndex,equation.length());
+            }
+            else{
+            equation = answer+equation.substring(secondIndex,equation.length());
+            }
+        System.out.println("Equation: " + equation);
+        System.out.println("----------------------------");
     }
 }
 
 public static void doSubtraction(){
-    
+    boolean putBack = false;
+    if(equation.indexOf("-")==0){
+        putBack = true;
+        equation = equation.replaceFirst("-","");
+    }
+    equation = equation.replace("-","+-");
+    if(putBack){
+        equation = "-"+equation;
+    }
+    System.out.println("Equation: " + equation);
 }
 
 public static void doMultiplication(){
+    boolean beginning = false;
+    while(equation.indexOf('*')!=-1){
+        firstIndex = equation.indexOf('*')-1;
+        //finds end index of first number
+        while(Character.isDigit(equation.charAt(firstIndex))||equation.charAt(firstIndex)==('.')){
+            if(firstIndex==1){
+                firstIndex--;
+                break;
+            } 
+            else if(firstIndex>0){
+                firstIndex--;
+            }
+            else{
+                break;
+            }
+        }
+        if(firstIndex == 0){
+            beginning = true;
+        }
+        System.out.println("firstIndex = " + firstIndex);
+        //creates first number
+        firstNum = Double.parseDouble(equation.substring(firstIndex, equation.indexOf('*')));
+        System.out.println("firstNum = " + firstNum);
 
+        secondIndex = equation.indexOf('*')+1;
+        //finds end index of first number
+        while(Character.isDigit(equation.charAt(secondIndex))||equation.charAt(secondIndex)==('.')||equation.charAt(secondIndex)==('-')){
+            if(secondIndex==equation.length()-1){
+                secondIndex++;
+                break;
+            }
+            else if(secondIndex<equation.length()){
+                secondIndex++;
+                System.out.println("2nd index = " + secondIndex);
+            } 
+            else{
+                break;
+            }
+        }
+        System.out.println("secondIndex = " + secondIndex);
+
+        //creates second number
+        secondNum = Double.parseDouble(equation.substring(equation.indexOf('*')+1,secondIndex));
+        System.out.println("secondNum = " + secondNum);
+
+        answer = (firstNum*secondNum);
+        System.out.println("answer = " + answer);
+        System.out.println("firstIndex: " + firstIndex + " " + "secondIndex: " + secondIndex + " " + "Boolean: " + beginning);
+
+        if(!beginning){
+        equation = equation.substring(0,firstIndex+1)+answer+equation.substring(secondIndex,equation.length());
+        }
+        else{
+        equation = answer+equation.substring(secondIndex,equation.length());
+        }
+        System.out.println("Equation: " + equation);
+        System.out.println("----------------------------");
+    } 
 }
 
 public static void doDivision(){
+    boolean beginning = false;
+    while(equation.indexOf('/')!=-1){
+        firstIndex = equation.indexOf('/')-1;
+        //finds end index of first number
+        while(Character.isDigit(equation.charAt(firstIndex))||equation.charAt(firstIndex)==('.')){
+            if(firstIndex==1){
+                firstIndex--;
+                break;
+            } 
+            else if(firstIndex>0){
+                firstIndex--;
+            }
+            else{
+                break;
+            }
+        }
+        if(firstIndex == 0){
+            beginning = true;
+        }
+        System.out.println("firstIndex = " + firstIndex);
+        //creates first number
+        firstNum = Double.parseDouble(equation.substring(firstIndex, equation.indexOf('/')));
+        System.out.println("firstNum = " + firstNum);
 
+        secondIndex = equation.indexOf('/')+1;
+        //finds end index of first number
+        while(Character.isDigit(equation.charAt(secondIndex))||equation.charAt(secondIndex)==('.')||equation.charAt(secondIndex)==('-')){
+            if(secondIndex==equation.length()-1){
+                secondIndex++;
+                break;
+            }
+            else if(secondIndex<equation.length()){
+                secondIndex++;
+                System.out.println("2nd index = " + secondIndex);
+            } 
+            else{
+                break;
+            }
+        }
+        System.out.println("secondIndex = " + secondIndex);
+
+        //creates second number
+        secondNum = Double.parseDouble(equation.substring(equation.indexOf('/')+1,secondIndex));
+        System.out.println("secondNum = " + secondNum);
+
+        answer = (firstNum/secondNum);
+        System.out.println("answer = " + answer);
+        System.out.println("firstIndex: " + firstIndex + " " + "secondIndex: " + secondIndex + " " + "Boolean: " + beginning);
+        if(!beginning){
+        equation = equation.substring(0,firstIndex+1)+answer+equation.substring(secondIndex,equation.length());
+        }
+        else{
+        equation = answer+equation.substring(secondIndex,equation.length());
+        }
+        System.out.println("Equation: " + equation);
+        System.out.println("----------------------------");
+    }
 }
 
 public static void doExponent(){
+    boolean beginning = false;
+    while(equation.indexOf('^')!=-1&&equation.indexOf('^')!=0){
+        firstIndex = equation.indexOf('^')-1;
+        //finds end index of first number
+        while(Character.isDigit(equation.charAt(firstIndex))||equation.charAt(firstIndex)==('.')){
+            //have to check if first number is negative
+            if(firstIndex==1){
+                firstIndex--;
+                break;
+            } 
+            else if(firstIndex>0){
+                firstIndex--;
+            }
+            else{
+                break;
+            }
+            //might need more conditions for else - break statement
+        }
+        if(firstIndex == 0){
+            beginning = true;
+        }
+        //creates first number
+        firstNum = Double.parseDouble(equation.substring(firstIndex, equation.indexOf('^')));
+        System.out.println("firstIndex = " + firstIndex);
+        System.out.println("firstNum = " + firstNum);
 
+        secondIndex = equation.indexOf('^')+1;
+        //finds end index of first number
+        while(Character.isDigit(equation.charAt(secondIndex))||equation.charAt(secondIndex)==('.')||equation.charAt(secondIndex)==('-')){
+            if(secondIndex==equation.length()-1){
+                secondIndex++;
+                break;
+            }
+            else if(secondIndex<equation.length()){
+                secondIndex++;
+            } 
+            else{
+                break;
+            }
+            //might need more conditions for else - break statement
+        }
+        //creates second number
+        secondNum = Double.parseDouble(equation.substring(equation.indexOf('^')+1,secondIndex));
+        System.out.println("secondIndex = " + secondIndex);
+        System.out.println("secondNum = " + secondNum);
+
+        //adds two numbers
+        answer = (Math.pow(firstNum,secondNum));
+        System.out.println("answer = " + answer);
+        System.out.println("firstIndex: " + firstIndex + " " + "secondIndex: " + secondIndex + " " + "Boolean: " + beginning);
+
+        //rewrites equation
+            if(!beginning){
+            equation = equation.substring(0,firstIndex+1)+answer+equation.substring(secondIndex,equation.length());
+            }
+            else{
+            equation = answer+equation.substring(secondIndex,equation.length());
+            }
+        System.out.println("Equation: " + equation);
+        System.out.println("----------------------------");
+    }
 }
 
 public static void doMath(){
 doExponent();
-doMultiplication();
 doDivision();
-doAddition();
+doMultiplication();
 doSubtraction();
+doAddition();
 }
 
 public static void main(String[] args){
-calculate();
+//calculate();
+quadraticSolver();
 }
 
 }
