534c2ca93d26af1314f22266f994e86cf6778e61
Kent213 <khocaoglu54@ladueschools.net>
2022-11-13 23:16:19 +0000

    sad face

--- website/GUI/GUI.java
+++ website/GUI/GUI.java
@@ -7,19 +7,24 @@ private JFrame frame;
 private JPanel panel;
 private JButton button;
 private JLabel label;
+private JLabel labelTwo;
 private int count = 0;
 
     public GUI(){
     frame = new JFrame();
     panel = new JPanel();
-    button = new JButton("Click Me");
+    button = new JButton("Don't Click Me");
     button.addActionListener(this);
-    label = new JLabel("Number of Clicks: 0");
+    label = new JLabel("");
+    labelTwo = new JLabel("");
 
-    panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+
+    panel.setBorder(BorderFactory.createEmptyBorder(25,40,10,40));
     panel.setLayout(new GridLayout(0,1));
     panel.add(button);
+    panel.add(labelTwo);
     panel.add(label);
+
 
     frame.add(panel, BorderLayout.CENTER);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,5 +39,6 @@ private int count = 0;
     public void actionPerformed(ActionEvent e){
         count++;
         label.setText("Number of clicks: " + count);
+        labelTwo.setText(labelTwo.getText()+"D: ");
     }
 }
