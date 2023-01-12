39f3e5b2829db358269be8fe7debed38768db665
Kent213 <khocaoglu54@ladueschools.net>
2023-01-12 13:25:51 +0000

    newStuff

---
+++ FolderOfStuff/caseSwitch.java
@@ -0,0 +1,18 @@
+package FolderOfStuff;
+
+public class caseSwitch {
+    public static void main (String[] args){
+        int n = 2;
+        switch(n) {
+            case 1:
+            System.out.println("nay");
+            break;
+            case 2:
+            System.out.println("yay");
+            break;
+            case 3:
+            System.out.println("nayy");
+            break;
+        }
+    }
+}
---
+++ FolderOfStuff/op.java
@@ -0,0 +1,11 @@
+package FolderOfStuff;
+import javax.security.auth.x500.X500Principal;
+
+public class op {
+    public static void main(String args[]){
+        int x = 5;
+        int y = 9;
+        int z = (x>y) ? x : y;
+        System.out.println(z);
+    }
+}
